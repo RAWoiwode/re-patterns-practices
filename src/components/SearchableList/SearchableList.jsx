7501ed9cd3f43a227059b6af02c4de0bc034eaa1
Ralph Woiwode <rawoiwode@gmail.com>
2024-03-13 12:48:17 -0500

    Adding SearchableList and simple search functionality to showcase Render Props

---
+++ src/components/SearchableList/SearchableList.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const SearchableList = ({ items }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const searchResults = items.filter((item) =>
+    JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div className="searchable-list">
+      <input type="search" placeholder="Search" onChange={handleChange} />
+      <ul>
+        {searchResults.map((item, index) => (
+          <li key={index}>{item.toString()}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default SearchableList;
